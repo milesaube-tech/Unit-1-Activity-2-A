1762aac9f7b536eae307375c671655445e8be2fe
default <miles.aube@stu.ocsb.ca>
2025-09-16 22:46:04 +0000

    added inital versions of go and main ts of Hello, World! programs

---
+++ main.go
@@ -0,0 +1,17 @@
+/*
+* Author: Miles Aube 
+* Version: 1.0.0
+* Date: 2025-09-16
+* This is my Hello, World! go version program
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+		// print out my Hello, World!
+		fmt.Println("Hello, World!")
+
+		fmt.Println("\nDone")
+}
---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Miles Aube
+ * @version 1.0.0
+ * @date 2025-09-16
+ * @fileoverview This is my unit one hello world program
+ */
+
+// print out the greeting
+console.log("Hello, World!");
+
+console.log("\nDone");
